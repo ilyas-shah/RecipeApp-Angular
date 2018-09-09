4f665dbe8991e95bca18d011059bee9f592d3ba4
ilyas-shah <ilyas.shah3@live.com>
2018-09-09 11:12:47 +0530

    select a particular recipe

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipe/recipe.model';
 
 @Component({
   selector: 'app-recipe',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
+  @Input() selectedRecipe: Recipe;
 
   constructor() { }
 
