9c704b48bd0bd56939ae80573dcc7be1d0c45e52
ilyas-shah <ilyas.shah@live.com>
2018-12-08 17:04:34 +0530

    added recipe subscribe

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe/recipe.model';
+import { RecipeService } from './recipe.service';
+
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.css']
+  styleUrls: ['./recipe.component.css'],
+  providers: [
+    RecipeService
+  ]
 })
 export class RecipeComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.selectedRecipe.subscribe(
+      (recipe: Recipe) => {
+        this.selectedRecipe = recipe;
+      }, (err) => {
+        console.log(err);
+      }
+    );
   }
 
 }
