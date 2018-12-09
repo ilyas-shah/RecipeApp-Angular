162b86b26216dff66303d888a155b7ed8050dbd4
ilyas-shah <ilyas.shah@live.com>
2018-12-09 15:40:58 +0530

    added addToShoppingList method.

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe/recipe.model';
+import { ShoppingService } from 'src/app/shoppinglist/shoppinglist.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,8 +11,15 @@ import { Recipe } from '../../recipe/recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppingService: ShoppingService) { }
 
   ngOnInit() {
+  }
+
+  addToShoppingList(ingredients: Ingredient[]) {
+    // console.log(ingredients);
+    ingredients.map( (ingredient: Ingredient) => {
+      this.shoppingService.addIngredient(ingredient);
+    });
   }
 }
