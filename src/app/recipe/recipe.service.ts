e659548a42b7c52107b40fe365c68d1a59224d5e
ilyas-shah <ilyas.shah@live.com>
2018-12-09 15:40:11 +0530

    added ingredients to recipe store

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shoppinglist/ingredient.model';
 
 @Injectable()
 
@@ -7,8 +8,24 @@ export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Nice Recipe', 'from the most popular chef', 'https://purewows3.imgix.net/images/articles/2017_11/cranberry-balsamic-roast-chicken-9211.jpg?auto=format,compress&cs=strip'),
-    new Recipe('Another Recipe', 'from the most unpopular chef', 'https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
+    new Recipe(
+      'Nice Recipe',
+      'from the most popular chef',
+      'https://purewows3.imgix.net/images/articles/2017_11/cranberry-balsamic-roast-chicken-9211.jpg?auto=format,compress&cs=strip',
+      [
+        new Ingredient('mushroom', 4),
+        new Ingredient('tamrind', 15)
+      ]
+    ),
+    new Recipe(
+      'Another Recipe',
+      'from the most unpopular chef',
+      'https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg',
+      [
+        new Ingredient('pomegranate', 20),
+        new Ingredient('whey', 25)
+      ]
+    )
   ];
 
   getRecipes() {
